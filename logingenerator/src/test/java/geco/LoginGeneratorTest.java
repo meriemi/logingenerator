a35ec877e66d75eb1127ac49fdaa462063781135
meriemi <meriem.farouj@gmail.com>
2017-11-23 09:31:53 +0100

    #couverture 100% de GenLogin

--- logingenerator/src/test/java/geco/LoginGeneratorTest.java
+++ logingenerator/src/test/java/geco/LoginGeneratorTest.java
@@ -7,19 +7,36 @@ import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
 
-    private LoginGenerator aLoginGenerator;
+    private LoginGenerator loginGenerator;
 
     @Before
     public void setUp() throws Exception {
         String[] start = new String[]{ "JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"};
-        aLoginGenerator = new LoginGenerator(new LoginService(start));
+        loginGenerator = new LoginGenerator(new LoginService(start));
     }
 
     @Test
     public void generateLoginForNomAndPrenom() throws Exception {
-        String login = aLoginGenerator.generateLoginForNomAndPrenom("Durand","Paul");
+        String login = loginGenerator.generateLoginForNomAndPrenom("Durand","Paul");
         assertEquals("PDUR",login);
     }
 
-    
+    @Test
+    public void generateLoginForJRAL2() throws Exception {
+        String login = loginGenerator.generateLoginForNomAndPrenom("Ralling","John");
+        assertEquals("JRAL2",login);
+    }
+
+    @Test
+    public void loginPDUR2() {
+        String login = loginGenerator.generateLoginForNomAndPrenom("Dùrand","Paul");
+        assertEquals("PDUR",login);
+    }
+
+    @Test
+    public void loginJROLNumber() {
+        String login = loginGenerator.generateLoginForNomAndPrenom("Rolling","Jean");
+        assertEquals("JROL1",login);
+    }
+
 }
